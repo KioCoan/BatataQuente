cb1a6698148f79892864778f899134369f5cfdcf
Henrique <henrique.plima1@gmail.com>
2014-04-24 16:15:12 -0300

    atualização erro de conexão facebook

--- TelaInicialViewController.h
+++ TelaInicialViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <FacebookSDK/FacebookSDK.h>
+#import "Reachability.h"
 
 @interface TelaInicialViewController : UIViewController <FBLoginViewDelegate>
 
--- TelaInicialViewController.m
+++ TelaInicialViewController.m
@@ -33,8 +33,8 @@
     
     
     [self.loginView sizeToFit];
-    //self.loginView.readPermissions = @[@"basic_info",@"email",@"user_likes",@"user_photos"];
-    self.loginView.readPermissions = @[@"basic_info"];
+    self.loginView.readPermissions = @[@"basic_info",@"email",@"user_likes",@"user_photos"];
+    //self.loginView.readPermissions = @[@"basic_info"];
     
     //Customiza foto do perfil
     
@@ -45,12 +45,75 @@
     
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [self verificaConexao];
+}
+
+- (void)configuracaoObjetosFacebook: (BOOL)opcao{
+    self.loginView.hidden = opcao;
+    self.fotoPerfil.hidden = opcao;
+    self.lblNomeUsuario.hidden = opcao;
+}
+
+
+
+
 - (void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user{
     
     
-    self.lblNomeUsuario.text = user.name;
-    self.fotoPerfil.profileID = user.id;
     
+        self.lblNomeUsuario.text = user.name;
+        self.fotoPerfil.profileID = user.id;
+    
+    
+    
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView{
+    self.fotoPerfil.profileID = nil;
+    self.lblNomeUsuario.text = @"Não Logado";
+}
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error{
+    NSString *alertMessage, *alertTitle;
+    
+    if ([FBErrorUtility shouldNotifyUserForError:error]) {
+        alertTitle = @"Erro no Facebook";
+        alertMessage = [FBErrorUtility userMessageForError:error];
+    }else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryAuthenticationReopenSession){
+        alertTitle = @"Erro na sessão";
+        alertMessage = @"A sessão atual não é mais válida. Por favor faça login novamente";
+    }else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled){
+        NSLog(@"usuario cancelou o login");
+    }else{
+        alertTitle = @"Ocorreu um erro";
+        alertMessage = @"tente de novo mais tarde";
+        NSLog(@"Unexpected error: %@",error);
+    }
+    
+    if (alertMessage) {
+        [[[UIAlertView alloc]initWithTitle:alertTitle message:alertMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]show];
+    }
+}
+
+- (BOOL)verificaConexao{
+    
+    Reachability *networkReability = [Reachability reachabilityForInternetConnection];
+    NetworkStatus networkStatus = [networkReability currentReachabilityStatus];
+    
+    
+    if (networkStatus == NotReachable) {
+        // nao existe conexao
+       // UIAlertView *alert = [[[UIAlertView alloc]initWithTitle:@"Sem conexão a internet" message:@"Conecte primeiro na internet" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]show];
+        
+        
+        
+        [[[UIAlertView alloc]initWithTitle:@"Sem conexão a internet" message:@"Conecte primeiro na internet" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]show];
+        
+        return NO;
+    }
+    return YES;
     
 }
 
